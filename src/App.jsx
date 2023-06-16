63a4262f69a0613047f141a09cf1e29a19e5c991
Francisco Lima <francisco.lima@liferay.com>
2023-06-16 16:09:21 -0300

    add countries list feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PageNotFound from './pages/PageNotFound'
 import AppLayout from "./pages/AppLayout"
 import Login from "./pages/Login"
 import CityList from "./components/CityList"
+import CountryList from "./components/CountryList"
 import { useState, useEffect } from "react"
 
 const BASE_URL = "http://localhost:8000"
@@ -42,7 +43,7 @@ function App() {
         <Route path="app" element={<AppLayout />} >
           <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
-          <Route path="countries" element={<p>countries</p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<p>map</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
