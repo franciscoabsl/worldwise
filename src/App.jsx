9212ebd6082dd14a3ff00222f28d7108319d640d
Francisco Lima <francisco.lima@liferay.com>
2023-06-16 14:23:46 -0300

    add components and basic app layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pricing from './pages/Pricing'
 import Homepage from './pages/Homepage'
 import PageNotFound from './pages/PageNotFound'
 import AppLayout from "./pages/AppLayout"
+import Login from "./pages/Login"
 
 function App() {
 
@@ -12,8 +13,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
