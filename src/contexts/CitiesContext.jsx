8d59b0d5d4ba1e407eb5abef34402df01b41f562
Francisco Lima <francisco.lima@liferay.com>
2023-06-19 13:49:29 -0300

    add useCities hook

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const BASE_URL = "http://localhost:8000"
 
@@ -33,4 +33,10 @@ function CitiesProvider({ children }) {
   </CitiesContext.Provider>
 }
 
-export { CitiesProvider }
+function useCities() {
+  const context = useContext(CitiesContext)
+  if(context === undefined) throw new Error("CitiesContex was used outside de CitiesProvider")
+  return context
+}
+
+export { CitiesProvider,useCities }
