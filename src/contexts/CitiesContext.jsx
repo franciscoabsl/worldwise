aa2c6d71bc07eb35f356fdb56b85cf6e6875e48a
Francisco Lima <francisco.lima@liferay.com>
2023-06-19 22:28:52 -0300

    add fake authentication

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -39,13 +39,15 @@ function reducer(state, action) {
 			return {
         ...state,
         isLoading: false,
-        cities: [...state.cities, action.payload]
+        cities: [...state.cities, action.payload],
+        currentCity: action.payload
       };
 		case 'city/deleted':
 			return {
         ...state,
         isLoading: false,
-        cities: state.cities.filter((city) => city.id !== action.payload)
+        cities: state.cities.filter((city) => city.id !== action.payload),
+        currentCity: {}
       }
 		case 'rejected':
 			return {
@@ -86,6 +88,7 @@ function CitiesProvider({children}) {
 	}, []);
 
 	async function getCity(id) {
+    if (Number(id) === currentCity.id) return;
     dispatch({type: 'loading'});
 		try {
 			
